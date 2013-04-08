184614934a459b25ae64e969bd5b4a03242af11b
Delian Asparouhov <me@delian.io>
2013-04-08 11:04:57 -0400

    Add in navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Contactlens::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'contacts#index'
 
+  match '/approve' => 'contacts#approve'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
