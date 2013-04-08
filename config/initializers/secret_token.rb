6abda5923f5afb2e4a784816765b54237ff8e890
Delian Asparouhov <me@delian.io>
2013-04-08 10:25:47 -0400

    Init of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contactlens::Application.config.secret_token = '2d977c00ebfead11d15de16a3cdfdb4386e73391fcb85ab4b6fa88763a9ff056e061993d16ae2cbd1fe16fd818cde8bd1089265c31a8c06709b164ddf6290320'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Contactlens::Application.config.session_store :cookie_store, key: '_contactlens_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Contactlens::Application.config.session_store :active_record_store
