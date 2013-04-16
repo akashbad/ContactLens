409731f8484fcc3fc17659d4bef3d25eb0320486
akashbad <akashbad@mit.edu>
2013-04-16 17:33:11 -0400

    added filtering

--- app/assets/javascripts/grid.js
+++ app/assets/javascripts/grid.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
 	$('#contact-grid').isotope({
 		itemSelector: '.contact-card',
 		masonryHorizontal: {
@@ -12,12 +12,37 @@ $(function(){
     source: tags
   });
 
-  $('#search-button').click(function(e){
+  $('#search-bar').keyup(function(event)  {
+    if(event.keyCode == 13){
+      $('#search-button').click();
+    }
+  });
+
+  $('#search-button').click(function(event){
     var tag = $('#search-bar').val();
     appendTag(tag);
+    $('#search-bar').val('');
+    filter();
+  });
+
+  $('#search-tags').on('click', 'li a', function(event){
+    event.preventDefault();
+    $(this).parent().toggleClass("active");
+    filter();
   });
 
   function appendTag(tag){
-    $('#search-tags').append("<li class='active'><a href='#'>" + tag + "</a></li>")
+    $('#search-tags').append("<li class='active' data-filter='."+tag.toLowerCase()+"'><a href='#' >" + tag + "</a></li>")
   };
+
+  function filter(){
+    var isoFilters = [];
+    $("#search-tags li.active").each(function(index, elem){
+      isoFilters.push($(elem).attr('data-filter'));
+    });
+    $('#contact-grid').isotope({filter: isoFilters.join(', ')});
+  };
+
+
+
 })
