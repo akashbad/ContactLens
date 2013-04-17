0ba2fe30c30f193ce9edff95a75a04c101ad84fd
Delian Asparouhov <me@delian.io>
2013-04-17 00:42:56 -0400

    Tagging on approve screen

---
+++ app/assets/javascripts/approve.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  var tags = ['Investor', 'Beta', 'Advisor', 'Reporter']
+  $('#tags-input').typeahead({
+      source: tags
+    });
+  $('#tags-input').keyup(function(event)  {
+    if(event.keyCode == 13){
+      var tag = $('#tags-input').val();
+      appendTag(tag);
+      $('#tags-input').val('');
+    }
+  });
+  function appendTag(tag){
+    $('#tags').append("<li class='active' data-filter='."+tag.toLowerCase()+"'><a href='#' >" + tag + "</a></li>")
+  };
+})
