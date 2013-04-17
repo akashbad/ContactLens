a6b10830d4c569b1d99a502a8cf36e44691e2dc8
Delian Asparouhov <me@delian.io>
2013-04-17 02:19:00 -0400

    Buttons up top do stuff

--- app/assets/javascripts/approve.js
+++ app/assets/javascripts/approve.js
@@ -13,4 +13,55 @@ $(document).ready(function(){
   function appendTag(tag){
     $('#tags').append("<li class='active' data-filter='."+tag.toLowerCase()+"'><a>" + tag + "</a></li>")
   };
+
+  function bringBack() {
+    $('#tags').empty();
+    $('#approve-container').css("left","0px");
+    $('#approve-container').css("opacity","0");
+    $('#approve-container').show();
+    $('#approve-container').animate({
+      opacity: 1
+    }, 500, function() {
+    });
+  }
+  $("#approve").click(function() {
+    if ($('#tags').children().length > 0) {
+      $('#approve-container').animate({
+        left: '+=2000',
+        opacity: 0.1
+      }, 500, function() {
+        $('#approve-container').hide();
+        setTimeout(callback(), 2000);
+      });
+    } else {
+      alert("Please enter tags");
+    }
+  });
+  $("#reject").click(function() {
+    $('#approve-container').animate({
+      left: '-=2000',
+      opacity: 0.1
+    }, 500, function() {
+      $('#approve-container').hide();
+      setTimeout(callback(), 2000);
+    });
+  });
+  $("#later").click(function() {
+    $('#approve-container').rotate({
+        angle: 0,
+        animateTo: 0 + 720,
+        duration: 1900
+    });
+    $('#approve-container').animate({
+      opacity: 0
+    }, 1200, function() {
+      $('#approve-container').hide();
+      setTimeout(callback(), 2000);
+    });
+  });
+  function callback(){
+    return function(){
+      bringBack();
+    }
+  }
 })
