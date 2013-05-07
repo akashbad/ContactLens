5da12ffa2da8eeb900a3c11572430409a94b33c7
Delian Asparouhov <me@delian.io>
2013-05-07 04:50:51 -0400

    Save notes and prevent twitter rate limit fuck

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130507075550) do
+ActiveRecord::Schema.define(:version => 20130507084301) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20130507075550) do
     t.string   "twitter_handle"
     t.integer  "user_id"
     t.string   "picture"
+    t.string   "note"
   end
 
   create_table "history_items", :force => true do |t|
@@ -56,6 +57,8 @@ ActiveRecord::Schema.define(:version => 20130507075550) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "full_name"
+    t.string   "twitter_handle"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
