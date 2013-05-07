f5874c92181aef4ab914e606d38269bdec1859a1
akashbad <akashbad@mit.edu>
2013-05-07 04:33:43 -0400

    Added linkify util

--- app/assets/javascripts/backbone/contactlens.js
+++ app/assets/javascripts/backbone/contactlens.js
@@ -8,7 +8,8 @@ window.ContactLens = window.ContactLens || {
   Models: {},
   Collections: {},
   Routers: {},
-  Views: {}
+  Views: {},
+  Utils: {}
 };
 
 // Use mustache style templates.
@@ -28,3 +29,16 @@ $.fn.multiline = function(text){
     this.html(this.html().replace(/\n/g,'<br/>'));
     return this;
 };
+
+ContactLens.Utils.linkifyTweets = function (tweet) {
+    // process links and reply
+    tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@@#\/%?=~_|!:,.;]*[-A-Z0-9+&@@#\/%=~_|])/ig, function (url) {
+        return '<a href="' + url + '" target="_blank">' + url + '</a>';
+    }).replace(/#([_a-z0-9]+)/ig, function (reply) {
+        return reply.charAt(0) + '<a href="https://twitter.com/search/%23' + reply.substring(1) + '" target="_blank">' + reply.substring(1) + '</a>';
+    }).replace(/@([_a-z0-9]+)/ig, function (reply) {
+        return reply.charAt(0) + '<a href="https://twitter.com/' + reply.substring(1) + '" target="_blank">' + reply.substring(1) + '</a>';
+    });
+    return tweet;
+};
+
